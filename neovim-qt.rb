6d6326e6f5e61b12a8cb8b3170e31b058182ef91
Chao Jiang <c.jiang@me.com>
2019-05-28 15:02:55 +0100

    removed patch

--- neovim-qt.rb
+++ neovim-qt.rb
@@ -23,30 +23,6 @@ class NeovimQt < Formula
             prefix.install "bin/nvim-qt.app"
             bin.install_symlink prefix/"nvim-qt.app/Contents/MacOS/nvim-qt"
         end
-        # Create a runner script to extend 'PATH'
-        # 'nvim-qt' could not find 'nvim' in '/usr/local/bin' in macOS High Sierra
-        # even '/usr/local/bin' is listed in '/etc/paths'
-        script = File.new(prefix/"nvim-qt.app/Contents/MacOS/nvim-qt-runner.sh", "w")
-        script.puts("#!/bin/bash")
-        script.puts('BASE="$(cd $(dirname $0); pwd)"')
-        script.puts('(cd $HOME; PATH="/usr/local/bin:/usr/bin:/bin" "${BASE}/nvim-qt")')
-        script.close()
-        chmod('+x', prefix/"nvim-qt.app/Contents/MacOS/nvim-qt-runner.sh")
     end
-
-    # Patch Info.plist to use runner script instead
-    patch :DATA
 end
 __END__
-index f4f4754..dd4d38a 100644
---- a/cmake/MacOSXBundleInfo.plist.in
-+++ b/cmake/MacOSXBundleInfo.plist.in
-@@ -5,7 +5,7 @@
-    <key>CFBundleDevelopmentRegion</key>
-    <string>English</string>
-    <key>CFBundleExecutable</key>
--	<string>nvim-qt</string>
-+	<string>nvim-qt-runner.sh</string>
-    <key>CFBundleDisplayName</key>
-    <string>Neovim</string>
-    <key>CFBundleGetInfoString</key>
