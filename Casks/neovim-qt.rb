1091775ff384c2d34c2c411bec69b82376e0661b
Chao Jiang <c.jiang@me.com>
2020-08-22 18:03:30 +0800

    updated cask

--- Casks/neovim-qt.rb
+++ Casks/neovim-qt.rb
@@ -1,27 +1,28 @@
-class NeovimQt < Formula
-    desc "Neovim client library and GUI, in Qt5"
-    homepage "https://github.com/equalsraf/neovim-qt"
-    url "https://github.com/equalsraf/neovim-qt/releases/download/v0.2.16.1/neovim-qt.zip"
-    sha256 "ddb4492db03da407703fb0ab271c4eb060250d1a7d71200e2b3b981cb0de59de"
+cask "neovim-qt" do
+  version "0.2.16.1"
+  sha256 "ddb4492db03da407703fb0ab271c4eb060250d1a7d71200e2b3b981cb0de59de"
 
-    head "https://github.com/equalsraf/neovim-qt"
+  url "https://github.com/equalsraf/neovim-qt/releases/download/v0.2.16.1/neovim-qt.zip"
+  appcast "https://github.com/equalsraf/neovim-qt/releases/"
+  name "neovim-qt"
+  homepage "https://github.com/equalsraf/neovim-qt"
 
-    option "without-release", "Build neovim-qt without -DCMAKE_BUILD_TYPE=Release"
+  option "without-release", "Build neovim-qt without -DCMAKE_BUILD_TYPE=Release"
 
-    depends_on "cmake" => :build
-    depends_on "qt"
-    depends_on "neovim"
+  depends_on "cmake" => :build
+  depends_on "qt"
+  depends_on "neovim"
 
-    def install
-        mkdir "build" do
-            if build.without? "release"
-                system "cmake", ".."
-            else
-                system "cmake", "-DCMAKE_BUILD_TYPE=Release", ".."
-            end
-            system "make"
-            prefix.install "bin/nvim-qt.app"
-            bin.install_symlink prefix/"nvim-qt.app/Contents/MacOS/nvim-qt"
-        end
+  def install
+    mkdir "build" do
+      if build.without? "release"
+        system "cmake", ".."
+      else
+        system "cmake", "-DCMAKE_BUILD_TYPE=Release", ".."
+      end
+      system "make"
+      prefix.install "bin/nvim-qt.app"
+      bin.install_symlink prefix/"nvim-qt.app/Contents/MacOS/nvim-qt"
     end
+  end
 end
